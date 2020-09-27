dd7c5cb7be37bb656b903d015a6c9c6703fb3466
Audi Nugraha <audinue@gmail.com>
2020-09-27 20:21:37 +0700

    Update yate.js
    Fix else, catch
    Add case, default
    Add tagged template literal

--- yate.js
+++ yate.js
@@ -1,18 +1,29 @@
-const yate = template => new Function(
+const yate = (template, ...substitutions) => new Function(
     'input',
-    `const output=[];with(input){${template.split('\n').map(
+    `const output=[];with(input){${
+      (Array.isArray(template)
+        ? String.raw(template, ...substitutions)
+        : template)
+      .split('\n')
+      .map(
         line =>
           /^\s*end\b/.test(line)
             ? '}'
             : /^\s*(var|let|const|break|continue|return)\b/.test(line)
               ? line
-              : /^\s*(if|else|switch|for|while|try|catch|with|function)\b/.test(line)
-                ? line + '{'
-                : `output.push('${
-                    line.replace(/([\\'])/g, '\\$1')
-                      .replace(/{([^}]+)}/g, (_, expression) => `',(${
-                        expression.replace(/\\([\\'])/g, '$1')
-                      })??'','`)
-                  }\\n')`
-      ).join('\n')}}return output.join('')`
+              : /^\s*(case|default)\b/.test(line)
+                ? line + ':'
+                : /^\s*(if|switch|for|while|try|with|function)\b/.test(line)
+                  ? line + '{'
+                  : /^\s*(else|catch)\b/.test(line)
+                    ? '}' + line + '{'
+                    : `output.push('${
+                        line
+                          .replace(/([\\'])/g, '\\$1')
+                          .replace(/{([^}]+)}/g, (_, expression) => `',(${
+                            expression.replace(/\\([\\'])/g, '$1')
+                          })??'','`)
+                      }\\n')`
+      ).join('\n')
+    }}return output.join('')`
   )
