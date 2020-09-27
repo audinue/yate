3476f22ba4fc7ce1262214018bf0337932a43c7e
Audi Nugraha <audinue@gmail.com>
2020-09-27 18:10:42 +0700

    Create yate.js

---
+++ yate.js
@@ -0,0 +1,18 @@
+const yate = template => new Function(
+    'input',
+    `const output=[];with(input){${template.split('\n').map(
+        line =>
+          /^\s*end\b/.test(line)
+            ? '}'
+            : /^\s*(var|let|const|break|continue|return)\b/.test(line)
+              ? line
+              : /^\s*(if|else|switch|for|while|try|catch|with|function)\b/.test(line)
+                ? line + '{'
+                : `output.push('${
+                    line.replace(/([\\'])/g, '\\$1')
+                      .replace(/{([^}]+)}/g, (_, expression) => `',(${
+                        expression.replace(/\\([\\'])/g, '$1')
+                      }),'`)
+                  }\\n')`
+      ).join('\n')}}return output.join('')`
+  )
