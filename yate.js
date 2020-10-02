cc0c0140aa8d7471eb62446dcc8d26881591ca75
Audi Nugraha <audinue@gmail.com>
2020-10-03 02:27:49 +0700

    Escape HTML

--- yate.js
+++ yate.js
@@ -1,4 +1,5 @@
 const yate = (template, ...substitutions) => new Function(
+    'yate',
     'input',
     `const output=[];with(input){${
       (Array.isArray(template)
@@ -20,10 +21,28 @@ const yate = (template, ...substitutions) => new Function(
                     : `output.push('${
                         line
                           .replace(/([\\'])/g, '\\$1')
-                          .replace(/{([^}]+)}/g, (_, expression) => `',(${
-                            expression.replace(/\\([\\'])/g, '$1')
-                          })??'','`)
+                          .replace(/{([^}]+)}/g, (_, expression) => `',${
+                            expression.startsWith('=')
+                              ? `(${expression.substr(1).replace(/\\([\\'])/g, '$1')})??''`
+                              : `yate.html(${expression.replace(/\\([\\'])/g, '$1')})`
+                          },'`)
                       }\\n')`
       ).join('\n')
     }}return output.join('')`
-  )
+  ).bind(null, yate)
+
+yate.html = value => {
+  if (value === null || value === undefined) {
+    return ''
+  }
+  return String(value)
+    .replace(/[&<>"']/g, char => {
+      switch (char) {
+        case '&': return '&amp;'
+        case '<': return '&lt;'
+        case '>': return '&gt;'
+        case '"': return '&quot;'
+        case "'": return '&apos;'
+      }
+    })
+}
